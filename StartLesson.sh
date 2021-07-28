99592be20fc1ac9b09d2f1efaaabfbd3ed3143e5
ishtyaq123 <ishtyaq2017@gmail.com>
2021-07-28 21:17:42 +0100

    git push origin main

--- StartLesson.sh
+++ StartLesson.sh
@@ -3,9 +3,10 @@
 
 #for this line to work need to be in home directory
 cd Documents/AICore/Essentials
-
 git checkout main
 git pull https://github.com/life-efficient/Essentials.git main
+git add .
+git commit 
 git push origin main
 
 echo "enter name of new branch"
@@ -26,5 +27,4 @@ git add .
 git commit 
 git remote -v
 git push origin $branchName
-
 
